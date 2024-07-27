e8095b1dae3979e2e2744b58215a9ac406f8e8af
Blake LeBlanc <blake.leblanc1@gmail.com>
2024-07-27 12:39:01 -0500

    Fix syntax to call custom.options; Now, about the fzf.vim errors...

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -890,7 +890,6 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
-  { import = 'custom.options' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -915,3 +914,5 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+require 'custom.options'
