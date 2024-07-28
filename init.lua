f00bd853968f2633287c20bd920141ff56e95675
Blake LeBlanc <blake.leblanc1@gmail.com>
2024-07-28 15:02:02 -0500

    Restructure whick-key custom opts

--- init.lua
+++ init.lua
@@ -272,22 +272,23 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      -- require('which-key').setup()
+      require('which-key').setup {
 
-      -- Old spec
-      -- Document existing key chains
-      -- require('which-key').add {
-      --   { '<leader>c', group = '[C]ode' },
-      --   { '<leader>d', group = '[D]ocument' },
-      --   { '<leader>r', group = '[R]ename' },
-      --   { '<leader>s', group = '[S]earch' },
-      --   { '<leader>w', group = '[W]orkspace' },
-      --   { '<leader>t', group = '[T]oggle' },
-      --   { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      -- }
+        -- Old spec
+        -- Document existing key chains
+        -- require('which-key').add {
+        --   { '<leader>c', group = '[C]ode' },
+        --   { '<leader>d', group = '[D]ocument' },
+        --   { '<leader>r', group = '[R]ename' },
+        --   { '<leader>s', group = '[S]earch' },
+        --   { '<leader>w', group = '[W]orkspace' },
+        --   { '<leader>t', group = '[T]oggle' },
+        --   { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        -- }
 
-      -- Attempt at new spec
-      require('whick-key').add {
+        -- Attempt at new spec
+        -- require('whick-key').add {
         { '<leader>c', group = '[C]ode' },
         { '<leader>c_', hidden = true },
         { '<leader>d', group = '[D]ocument' },
