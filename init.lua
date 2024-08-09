39d56270d565ad12d77d7d25ae6fd3f4a0f793f3
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-08-09 17:11:38 -0500

    Minor restructure

--- init.lua
+++ init.lua
@@ -868,7 +868,14 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+    },
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
