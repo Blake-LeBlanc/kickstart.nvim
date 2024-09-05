717d87d39d8a28e56aae1acd63048837aa9e27fd
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-09-05 05:46:07 -0500

    Add nvim-surround plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -312,4 +312,14 @@ return {
       'nvim-lua/plenary.nvim',
     },
   },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- defaults
+      }
+    end,
+  },
 }
