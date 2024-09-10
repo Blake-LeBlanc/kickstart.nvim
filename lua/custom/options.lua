6ab535fae09994115b70d28d3c52b724c2dbac4e
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-09-10 08:55:59 -0500

    Move all opts to options.lua; refine keymaps

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,46 +1,56 @@
 local opt = vim.opt
 
-opt.foldmethod = 'marker'
-opt.viewdir = vim.fn.expand '$HOME/vimfiles/view'
-opt.hlsearch = true
-opt.incsearch = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.gdefault = true
-opt.textwidth = 100
-opt.colorcolumn = '+1'
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.shiftround = true
-opt.smarttab = true
-opt.showbreak = '...'
-opt.linebreak = true
-opt.modeline = false
 opt.autoindent = true
-opt.copyindent = true
-opt.breakindent = true
-opt.startofline = false
-opt.matchtime = 1
-opt.backspace = { 'indent', 'eol', 'start' }
 opt.autoread = true
-opt.ttimeoutlen = 10
-opt.ttyfast = true
-opt.statusline = '%<%f %h%m%r%=%-14.(%l,%c%V%) %P'
+opt.backspace = { 'indent', 'eol', 'start' }
+opt.backup = true
+opt.breakindent = true
+opt.colorcolumn = '+1'
+opt.copyindent = true
+opt.cursorline = true
+opt.foldmethod = 'marker'
+opt.gdefault = true
+opt.guicursor = ''
+opt.hlsearch = true
+opt.ignorecase = true
+opt.incsearch = true
+opt.inccommand = 'split'
 opt.laststatus = 2
-opt.report = 0
-opt.showmode = false
-opt.showcmd = true
-opt.shortmess:remove 'S'
-opt.shortmess:append 'F'
+opt.linebreak = true
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.matchtime = 1
+opt.modeline = false
+opt.mouse = 'a'
 opt.number = true
 opt.relativenumber = true
+opt.report = 0
+opt.scrolloff = 7
+opt.shiftround = true
+opt.shiftwidth = 2
+opt.shortmess:append 'F'
+opt.shortmess:remove 'S'
+opt.showbreak = '...'
+opt.showcmd = true
+opt.showmode = false
+opt.signcolumn = 'yes'
+opt.smartcase = true
+opt.smarttab = true
+opt.splitright = true
+opt.splitbelow = true
+opt.startofline = false
+opt.statusline = '%<%f %h%m%r%=%-14.(%l,%c%V%) %P'
+opt.swapfile = true
+opt.tabstop = 2
+opt.textwidth = 100
+opt.ttimeoutlen = 300
+opt.ttyfast = true
+opt.updatetime = 250
+opt.viewdir = vim.fn.expand '$HOME/vimfiles/view'
+opt.wildignore = { '.o', '.obj', '.bak', '.exe' }
 opt.wildmenu = true
 opt.wildmode = { 'list:longest' }
-opt.wildignore = { '.o', '.obj', '.bak', '.exe' }
-opt.backup = true
 opt.writebackup = true
-opt.swapfile = true
-opt.guicursor = ''
 
 -- Autocommands
 -- vim.cmd [[
@@ -85,11 +95,11 @@ keymap('n', 'A', 'A', opts)
 -- keymap('n', '<C-h>', ':History<CR>', opts)
 
 -- :Telescope equivalents of same FZF commands
-keymap('n', '<C-p>', ':Telescope git_files<CR>', opts)
-keymap('n', '<C-e>', ':Telescope find_files<CR>', opts)
-keymap('n', '<C-s>', ':Telescope live_grep<CR>', opts)
-keymap('n', '<C-b>', ':Telescope buffers<CR>', opts)
-keymap('n', '<C-h>', ':Telescope oldfiles<CR>', opts)
+-- keymap('n', '<C-p>', ':Telescope git_files<CR>', opts)
+-- keymap('n', '<C-e>', ':Telescope find_files<CR>', opts)
+-- keymap('n', '<C-s>', ':Telescope live_grep<CR>', opts)
+-- keymap('n', '<C-b>', ':Telescope buffers<CR>', opts)
+-- keymap('n', '<C-h>', ':Telescope oldfiles<CR>', opts)
 
 -- keymap('n', '<leader>fb', ':Telescope file_browser<CR>', opts)
 keymap('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', opts)
