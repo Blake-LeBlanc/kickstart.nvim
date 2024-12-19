3c0feb620195ee6d324f18a5a3a49fe4f45fd7cf
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-12-19 10:49:37 -0600

    Set python3_host_prog; Add keymap to cd current buf loc

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+local user_profile = vim.fn.getenv 'USERPROFILE'
+vim.g.python3_host_prog = user_profile .. '/.pyenv/pyenv-win/versions/3.13.1/python.exe'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -96,6 +96,7 @@ keymap('n', 'A', 'A', opts)
 keymap('n', '<leader>z', ':ZenMode<CR>', opts)
 keymap('n', '<leader>nt', ':Neotree toggle left reveal_force_cwd<CR><C-w>=', opts)
 -- keymap('n', '<leader>nt', ':Neotree toggle current reveal_force_cwd<CR>', opts)
+keymap('n', '<leader>cd', ':cd %:p:h<CR>', opts)
 
 -- :FZF related commands
 -- keymap('n', '<C-p>', ':GFiles<CR>', opts)
