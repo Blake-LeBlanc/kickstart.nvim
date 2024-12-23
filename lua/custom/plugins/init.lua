f6e97cd5ec9b2bc7421ff63e9c6deb084acb38f7
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-12-23 09:47:19 -0600

    Disable smear

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1124,12 +1124,12 @@ return {
   --     require('bullets').setup()
   --   end,
   -- },
-  {
-    'sphamba/smear-cursor.nvim',
-    opts = {
-      legacy_computing_symbols_support = false,
-    },
-  },
+  -- {
+  --   'sphamba/smear-cursor.nvim',
+  --   opts = {
+  --     legacy_computing_symbols_support = false,
+  --   },
+  -- },
   -- {
   --   'karb94/neoscroll.nvim',
   --   opts = {
