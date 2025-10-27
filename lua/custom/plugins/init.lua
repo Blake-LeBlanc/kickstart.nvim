f5ae038adf844f176a0336a3b9a076b5aaedccf5
Blake LeBlanc <blake.leblanc@guernsey.us>
2025-10-27 08:48:06 -0500

    Add tiny-inline-diagnostic plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -736,4 +736,14 @@ return {
   --   cmd = 'Store',
   --   opts = {},
   -- },}}}
+  -- {
+  --   'rachartier/tiny-inline-diagnostic.nvim',
+  --   event = 'VeryLazy',
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+  -- {
+  --   'neovim/nvim-lspconfig',
+  --   opts = { diagnostics = { virtual_text = false } },
+  -- },
 }
