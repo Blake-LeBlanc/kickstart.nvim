2afda3ff5b12fafaafc824e953df4250f82a5085
Blake LeBlanc <blake.leblanc1@gmail.com>
2026-03-08 09:18:37 -0500

    Update tree-sitter config

--- init.lua
+++ init.lua
@@ -644,31 +644,31 @@ require('lazy').setup({
           --    See `:help CursorHold` for information about when this is executed
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
-      -- So **THIS** is what has been highlighting the matching words! I can't stand that!
-      --     local client = vim.lsp.get_client_by_id(event.data.client_id)
-      --     if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-      --       local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-      --       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      --         buffer = event.buf,
-      --         group = highlight_augroup,
-      --         callback = vim.lsp.buf.document_highlight,
-      --       })
-      --
-      --       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      --         buffer = event.buf,
-      --         group = highlight_augroup,
-      --         callback = vim.lsp.buf.clear_references,
-      --       })
-      --
-      --       vim.api.nvim_create_autocmd('LspDetach', {
-      --         group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-      --         callback = function(event2)
-      --           vim.lsp.buf.clear_references()
-      --           vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-      --         end,
-      --       })
-      --     end
-         end,
+          -- So **THIS** is what has been highlighting the matching words! I can't stand that!
+          --     local client = vim.lsp.get_client_by_id(event.data.client_id)
+          --     if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+          --       local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          --       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+          --         buffer = event.buf,
+          --         group = highlight_augroup,
+          --         callback = vim.lsp.buf.document_highlight,
+          --       })
+          --
+          --       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+          --         buffer = event.buf,
+          --         group = highlight_augroup,
+          --         callback = vim.lsp.buf.clear_references,
+          --       })
+          --
+          --       vim.api.nvim_create_autocmd('LspDetach', {
+          --         group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+          --         callback = function(event2)
+          --           vim.lsp.buf.clear_references()
+          --           vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+          --         end,
+          --       })
+          --     end
+        end,
       })
 
       -- Diagnostic Config
@@ -1008,7 +1008,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
