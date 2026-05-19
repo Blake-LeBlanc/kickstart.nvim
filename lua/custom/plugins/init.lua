0c70ecf66c379a5cd64d4e82d4ee92dcb267be4f
Blake LeBlanc <blake.leblanc@guernsey.us>
2026-05-19 11:13:44 -0500

    Add fff search picker

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -910,4 +910,54 @@ return {
   --     require('nvim-bqf').setup {}
   --   end,
   -- },
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      require('fff.download').download_or_build_binary()
+    end,
+    opts = {
+      debug = {
+        enabled = true,
+        show_scores = true,
+      },
+    },
+    lazy = false,
+    keys = {
+      {
+        '<leader>ff',
+        function()
+          require('fff').find_files()
+        end,
+        desc = 'FFFind files',
+      },
+      {
+        '<leader>fg',
+        function()
+          require('fff').live_grep()
+        end,
+        desc = 'LiFFFe grep',
+      },
+      {
+        '<leader>fz',
+        function()
+          require('fff').live_grep {
+            modes = {
+              'fuzzy',
+              'plain',
+            },
+          }
+        end,
+        desc = 'Live fffuzy grep',
+      },
+      {
+        '<leader>fc',
+        function()
+          require('fff').live_grep {
+            query = vim.fn.expand '<cword>',
+          }
+        end,
+        desc = 'Search current word',
+      },
+    },
+  },
 }
